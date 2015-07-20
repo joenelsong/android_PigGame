421fd685da615f6bb63f5a66ccf8a10382a9c029
Joseph <joenelsong@gmail.com>
2015-07-20 11:56:52 -0700

    minor changes
    Saving state of player names on first fragment
    Removed a bunch of stuff from first fargment, it should be in the second
    fragment or main activity instead.

--- Assignment3_PigGame/app/src/main/java/com/example/jnelson/assignment2_piggame/FirstFragment.java
+++ Assignment3_PigGame/app/src/main/java/com/example/jnelson/assignment2_piggame/FirstFragment.java
@@ -19,13 +19,10 @@ import android.widget.Toast;
 public class FirstFragment extends Fragment
                            implements OnClickListener {
     private boolean logging = true;
+    private boolean twoPaneLayout;
 
-    private int PLAYTOSCORE = 100;
-    private int SIDESONDIE = 6;
-    private int NUMBEROFDICE = 1;
     private PigGame game; // Game Object
 
-    private boolean twoPaneLayout;
     private EditText player1Name;
     private EditText player2Name;
     private Button startGame;
@@ -51,37 +48,28 @@ public class FirstFragment extends Fragment
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        // Get a references from the host activity
-        activity = (FirstActivity)getActivity();
+        activity = (FirstActivity) getActivity(); // Get a references from the host activity
+        /* ************************************************************** *
+        *                     Load Saved Player Names                     *
+        * *************************************************************** */
+        if (savedInstanceState != null) {// Load Player and Temporary round scores
+            player1Name.setText(savedInstanceState.getString("player1namesave", "@string/default_name"));
+            player2Name.setText(savedInstanceState.getString("player2namesave", "@string/default_name"));
 
-        // Make a new game object, use saved state if it exists
-        if(savedInstanceState != null) {// Load Player and Temporary round scores
-            game.getPlayers()[0].setmScore(savedInstanceState.getInt("mPlayer1_Score_save"));
-            game.getPlayers()[1].setmScore(savedInstanceState.getInt("mPlayer2_Score_save"));
-            game.setmTempScore(savedInstanceState.getInt("mTempScore_save"));
-            ////UpdateScores(); // Update Scores UIs
-
-            game.setmActivePlayerIndex(savedInstanceState.getInt("mActivePlayerIndex_Save"));
-            //UpdateActivePlayer(); // Update UI
-
-            game.setmDie(savedInstanceState.getInt("mDie_Save1"),0);
-            //if (NUMBEROFDICE > 1) { game.setmDie(savedInstanceState.getInt("mDie_Save2"),1); }
-            //if (NUMBEROFDICE > 1) { game.setmDie(savedInstanceState.getInt("mDie_Save3"),2); }
-            ////UpdateDie(mDice[0], 1); // Update Die UI
-
-            game.setmWinnerIndex(savedInstanceState.getInt("mWinnerIndex_save")); //Update Game State
-
+            // Check to see if FirstActivity has loaded a single or dual pane layout
+            twoPaneLayout = activity.findViewById(R.id.second_fragment) != null;
         }
-        if (logging) Log.d("FirstFragment", "Finish: OnActivityCreate()");
+    }
 
-        else {
-            game = new PigGame(PLAYTOSCORE, SIDESONDIE);
-        }
-        // Give the host activity a reference to the game object
-        activity.setGame(game);
-
-        // Check to see if FirstActivity has loaded a single or dual pane layout
-        twoPaneLayout = activity.findViewById(R.id.second_fragment) != null;
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        /* ************************************************************** *
+        *                     Save Player Names                           *
+        * *************************************************************** */
+        super.onSaveInstanceState(outState);
+        outState.putString("player1namesave", player1Name.getText().toString());
+        outState.putString("player2namesave", player1Name.getText().toString());
     }
 
     @Override
